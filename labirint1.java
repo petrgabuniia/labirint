2f765dd28118a41480258b1e408d146ee61b9640
petr gabuniia 231RDB331 <petr,gabuniya@gmail.com>
2023-11-30 13:16:43 +0200

    created array

---
+++ labirint1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class labirint1 {
+
+        public static void main(String[] args) {
+        int R, L;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lūdzu, ievediet labirinta garumu");
+        R = sc.nextInt();
+        System.out.println("Lūdzu, ievediet labirinta augstumu");
+        L = sc.nextInt();
+        boolean labirints [][] = new boolean [R][L];
+
+        }
+}
+
+        
