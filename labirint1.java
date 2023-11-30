d4877e2bb1444c45ea81df4a8d3a0e2661ac528b
ValentinsKoposovs <144010649+ValentinsKoposovs@users.noreply.github.com>
2023-11-30 15:32:23 +0200

    Update labirint1.java
    True/False uz 1/0

--- labirint1.java
+++ labirint1.java
@@ -10,47 +10,53 @@ public class labirint1 {
         R = sc.nextInt();
         System.out.println("Lūdzu, ievediet labirinta augstumu");
         L = sc.nextInt();
-        boolean labirints [][] = new boolean [R][L];
-        boolean filling; // true - manuāli, false - automātiski
-        System.out.println("Vai vēlaties aizpildīt labirintu manuāli? (true/false)");
-        filling = sc.nextBoolean();
-        if (filling) {
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < L; j++) {
-                    System.out.println("Lūdzu, ievadiet " + i + ". rindas " + j + ". kolonnas vērtību (true/false)");
-                    labirints[i][j] = sc.nextBoolean();
-                }
-            }
-        } else {
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < L; j++) {
-                    labirints[i][j] = Math.random() < 0.3;
+        int labirints [][] = new int [R][L];
+        int filling; // 1 - manuāli, 0 - automātiski
+        System.out.println("Vai vēlaties aizpildīt labirintu manuāli? (1 - jā, 0 - nē)");
+        filling = sc.nextInt();
+        if (filling == 1)
+        {
+            for (int i = 0; i < R; i++)
+            {
+                for (int j = 0; j < L; j++)
+                {
+                    System.out.println("Lūdzu, ievadiet " + (i + 1) + ". rindas " + (j + 1) + ". kolonnas vērtību (1 - siena, 0 - ceļš)");
+                    labirints[i][j] = sc.nextInt();
                 }
             }
         }
-        labirints[0][0] = true;
-        labirints[R-1][L-1] = true;
+        else
+        {
+            for (int i = 0; i < R; i++)
+            {
+                for (int j = 0; j < L; j++)
+                {
+                    labirints[i][j] = (int)(Math.random()*10 % 2);
+                }
+            }
+        }
+        labirints[0][0] = 0;
+        labirints[R-1][L-1] = 0;
         printLabyrinth(labirints);
-        
-
-        
-
-
     }
 
-    public static void printLabyrinth(boolean[][] labyrinth) {
-        for (int i = 0; i < labyrinth.length; i++) {
-            for (int j = 0; j < labyrinth[i].length; j++) {
-                if (labyrinth[i][j]) {
-                    System.out.print("  "); // Entrance, exit, or open path
-                } else {
-                    System.out.print("X "); // Wall
+
+    public static void printLabyrinth(int[][] labyrinth)
+    {
+        for (int i = 0; i < labyrinth.length; i++)
+        {
+            for (int j = 0; j < labyrinth[i].length; j++)
+            {
+                if (labyrinth[i][j] == 0)
+                {
+                    System.out.print("0 "); // Entrance, exit, or open path
+                }
+                else
+                {
+                    System.out.print("1 "); // Wall
                 }
             }
             System.out.println();
         }
     }
-
 }
-
-        
