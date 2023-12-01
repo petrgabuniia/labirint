a15309ea9d511f5e339dd7fad5ff2aaa93652b65
tomasskristianssterns <tomass-kristians.sterns@edu.rtu.lv>
2023-12-01 12:10:32 +0200

    Create test cases

---
+++ MainTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+
+public class MainTest {
+
+    @Test
+    public void testBruteForce() {
+        Main main = new Main();
+        int[][] labyrinth = {
+            {0, 0, 0},
+            {1, 1, 0}
+        };
+
+        // Test the bruteForce method
+        assertArrayEquals( , main.bruteForce(labyrinth)); 
+
+    }
+}
