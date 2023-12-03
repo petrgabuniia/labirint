0688e43cb03047567428df2f0dad863383db833f
tomasskristianssterns <tomass-kristians.sterns@edu.rtu.lv>
2023-12-03 09:39:52 +0200

    Update test cases

--- MainTest.java
+++ MainTest.java
@@ -7,18 +7,16 @@ public class MainTest {
 
     @Test //Tomass
     public void testBruteForce1() {
-        Main main = new Main();
         int[][] labyrinth = {
             {0, 0, 0},
             {1, 1, 0}
         };
         
-        assertEquals("(0,0) (0,1) (0,2) (1,2)", main.bruteForce(labyrinth)); // expected value, actual value
+        assertEquals("(0,0) (0,1) (0,2) (1,2)", Main.bruteForce(labyrinth)); // expected value, actual value
     }
 
     @Test //Tomass
-    public void testBruteForce2() { 
-        Main main = new Main();
+    public void testBruteForce2() {
         int[][] labyrinth = {
             {0, 0, 0, 0},
             {1, 1, 0, 1},
@@ -26,12 +24,11 @@ public class MainTest {
             {1, 1, 1, 0}
         };
         
-        assertEquals("(0,0) (0,1) (0,2) (1,2) (2,2) (2,3) (3,3)", main.bruteForce(labyrinth)); 
+        assertEquals("(0,0) (0,1) (0,2) (1,2) (2,2) (2,3) (3,3)", Main.bruteForce(labyrinth)); 
     }
 
     @Test //Tomass
-    public void testBruteForce3() { 
-        Main main = new Main();
+    public void testBruteForce3() {
         int[][] labyrinth = {
             {0, 0, 0, 0},
             {1, 1, 1, 0},
@@ -40,14 +37,13 @@ public class MainTest {
             {1, 0, 1, 0}
         };
         
-        assertEquals("Exit not found!", main.bruteForce(labyrinth)); 
+        assertEquals("Exit not found!", Main.bruteForce(labyrinth)); 
     }
 
 
 
     @Test //Tomass
-    public void testBruteForce4() { 
-        Main main = new Main();
+    public void testBruteForce4() {
         int[][] labyrinth = {
             {0, 1, 0, 0, 0},
             {0, 1, 0, 1, 0},
@@ -56,6 +52,6 @@ public class MainTest {
             {0, 0, 1, 1, 0}
         };
         
-        assertNotEquals("Exit not found!", main.bruteForce(labyrinth)); 
+        assertNotEquals("Exit not found!", Main.bruteForce(labyrinth)); 
     }
 }
