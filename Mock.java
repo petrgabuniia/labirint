69aea817402c1cbfd88ae3db85c4cf0052410fdf
petr gabuniia 231RDB331 <petr,gabuniya@gmail.com>
2023-12-06 12:30:40 +0200

    dfs finding any way, mock-only shortest

--- Mock.java
+++ Mock.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
-public class Mock {
+public class mock {
+
     private static class LabyrinthGraph {
-        private Map<Integer, List<VertexInfo>> adjacencyList;
+        private Map<Integer, Set<VertexInfo>> adjacencyList;
 
         public LabyrinthGraph() {
             this.adjacencyList = new HashMap<>();
         }
 
         public void addEdge(int v, int w, int steps) {
-            adjacencyList.computeIfAbsent(v, k -> new ArrayList<>()).add(new VertexInfo(w, steps));
-            adjacencyList.computeIfAbsent(w, k -> new ArrayList<>()).add(new VertexInfo(v, steps));
+            adjacencyList.computeIfAbsent(v, k -> new HashSet<>()).add(new VertexInfo(w, steps));
         }
 
         private static class VertexInfo {
@@ -26,70 +26,90 @@ public class Mock {
 
     public static void main(String[] args) {
         int[][] labyrinth = {
-                {1, 1, 0, 0},
-                {1, 0, 1, 1},
-                {1, 1, 1, 0},
-                {0, 1, 1, 1}
+                {1, 0, 1, 1, 1},
+                {1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1},
+                {1, 0, 1, 1, 0},
+                {1, 1, 1, 0, 1}
         };
 
-        int[][] targetCells = {
-                {2, 0},
-                {3, 3}
-                // Add more target cells as needed
-        };
-
-        createEdges(labyrinth, targetCells);
+        findShortestPath(labyrinth);
     }
 
-    static void createEdges(int[][] labyrinth, int[][] targetCells) {
+    static void findShortestPath(int[][] labyrinth) {
         int rows = labyrinth.length;
         int cols = labyrinth[0].length;
 
         LabyrinthGraph graph = new LabyrinthGraph();
-        boolean[][] visited = new boolean[rows][cols];
 
-        for (int[] targetCell : targetCells) {
-            int targetI = targetCell[0];
-            int targetJ = targetCell[1];
-
-            dfs(0, 0, labyrinth, graph, visited, rows, cols, 0, targetI, targetJ);
-        }
-
-        // Print information about the steps to reach all target cells
-        for (Map.Entry<Integer, List<Mock.LabyrinthGraph.VertexInfo>> entry : graph.adjacencyList.entrySet()) {
-            int vertex = entry.getKey();
-            int i = vertex / cols;
-            int j = vertex % cols;
-
-            for (Mock.LabyrinthGraph.VertexInfo vertexInfo : entry.getValue()) {
-                int targetI = vertexInfo.vertex / cols;
-                int targetJ = vertexInfo.vertex % cols;
-
-                System.out.println("To reach target cell (" + targetI + ", " + targetJ + ") from (" + i + ", " + j +
-                        ") you should make " + vertexInfo.steps + " steps.");
+        // Add edges between adjacent cells
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (labyrinth[i][j] == 1) {
+                    if (j + 1 < cols && labyrinth[i][j + 1] == 1) {
+                        int vertex1 = i * cols + j;
+                        int vertex2 = i * cols + (j + 1);
+                        graph.addEdge(vertex1, vertex2, 1);
+                        graph.addEdge(vertex2, vertex1, 1);
+                    }
+                    if (i + 1 < rows && labyrinth[i + 1][j] == 1) {
+                        int vertex1 = i * cols + j;
+                        int vertex2 = (i + 1) * cols + j;
+                        graph.addEdge(vertex1, vertex2, 1);
+                        graph.addEdge(vertex2, vertex1, 1);
+                    }
+                }
             }
         }
+
+        
+        int[] distances = new int[rows * cols];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[0] = 0;
+
+        int[] prevVertices = new int[rows * cols];
+        Arrays.fill(prevVertices, -1);
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(vertex -> distances[vertex]));
+        queue.offer(0);
+
+        while (!queue.isEmpty()) {
+            int currentVertex = queue.poll();
+
+            for (LabyrinthGraph.VertexInfo neighborInfo : graph.adjacencyList.getOrDefault(currentVertex, Collections.emptySet())) {
+                int neighbor = neighborInfo.vertex;
+                int weight = neighborInfo.steps;
+
+                if (distances[currentVertex] + weight < distances[neighbor]) {
+                    distances[neighbor] = distances[currentVertex] + weight;
+                    prevVertices[neighbor] = currentVertex;
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        printShortestPath(prevVertices, rows, cols, distances[rows * cols - 1]);
     }
 
-    static boolean dfs(int i, int j, int[][] labyrinth, LabyrinthGraph graph, boolean[][] visited, int rows, int cols, int steps, int targetI, int targetJ) {
-        if (i < 0 || i >= rows || j < 0 || j >= cols || visited[i][j] || labyrinth[i][j] == 0) {
-            return false;
+    static void printShortestPath(int[] prevVertices, int rows, int cols, int shortestPathLength) {
+        List<String> path = new ArrayList<>();
+        int currentVertex = rows * cols - 1;
+
+        while (currentVertex != -1) {
+            int i = currentVertex / cols;
+            int j = currentVertex % cols;
+            path.add("(" + i + ", " + j + ")");
+            currentVertex = prevVertices[currentVertex];
         }
 
-        int vertex = i * cols + j;
-        visited[i][j] = true;
+        Collections.reverse(path);
 
-        if (i == targetI && j == targetJ) {
-            // Reached a target cell, add information to the graph
-            graph.addEdge(vertex, vertex, steps);
-            return true;
+        if (shortestPathLength == Integer.MAX_VALUE) {
+            System.out.println("No path found");
+            return;
         }
-
-        if (dfs(i, j + 1, labyrinth, graph, visited, rows, cols, steps + 1, targetI, targetJ) ||
-                dfs(i + 1, j, labyrinth, graph, visited, rows, cols, steps + 1, targetI, targetJ)) {
-            return true;
-        }
-
-        return false;
+        System.out.println("Shortest Path found");
+        System.out.println("Length of the Shortest Path: " + shortestPathLength);
+        System.out.println("Path: " + String.join(" -> ", path));
     }
 }
